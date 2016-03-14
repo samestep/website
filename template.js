a39f4b479d85e89d0efb4be3ff01062446fa0407
samestep <samestep@users.noreply.github.com>
2016-03-13 20:00:29 -0400

    Rework template

--- template.js
+++ template.js
@@ -1,7 +1,8 @@
 $(function() {
   var content = $('body > *').detach();
   $('body').load('/template.html', function() {
-    content.appendTo('#body');
-    $('#nav-' + $('body').data('nav')).attr('class', 'current-nav');
+    $('header').clone().addClass('placeholder').prependTo('body');
+    $('header nav a.' + $('body').data('nav')).addClass('current');
+    content.appendTo('main');
   });
 });
