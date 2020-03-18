3b6d2851a6cea8c0f73b108d4c98e160690c0ed4
Sam Estep <sam@samestep.com>
2020-03-18 08:20:16 -0400

    Remove everything
    Eventually I'll put a real website here...

--- template.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  var content = $('body > *').detach();
-  $('body').load('/template.html', function() {
-    $('header').clone().addClass('placeholder').prependTo('body');
-    $('header nav a.' + $('body').data('nav')).addClass('current');
-    content.appendTo('main');
-  });
-});
